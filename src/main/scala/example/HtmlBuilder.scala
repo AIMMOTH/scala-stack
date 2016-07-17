1e4ca74c03227a9e2fcb74a574a62ca5904d3375
Carl <carl@localhost>
2016-07-17 23:26:40 +0200

    ISO Scala - Succesful Angular example!

--- src/main/scala/example/HtmlBuilder.scala
+++ src/main/scala/example/HtmlBuilder.scala
@@ -11,33 +11,43 @@ import javax.servlet.annotation.WebServlet
 
 object Html {
 
-  def apply(css : String, minified : Boolean = false) = {
+  def apply(css: String, minified: Boolean = false) = {
     val min = if (minified) ".min" else ""
     html(
       head(
         title2("All Scala!"),
         link(rel := "stylesheet", href := s"/css/foundation$min.css"),
         link(rel := "stylesheet", href := "/css/app.css"),
-        style2(`type` := "text/css")(css)
-//        script(src := "javascript.js")
-        ),
-      body(
+        style2(`type` := "text/css")(css)),
+      body(attr("ng-app") := "app")(
         div(cls := "row")(
           div(cls := "large-12 columns")(
             h1("All Scala!"),
             p(backgroundColor := "green")("Push the red button below for an alert!"),
             button(cls := "example-Stylisch-redBackground example-Stylisch-yellowColor")(onclick := "example.HelloWorld().alert()")("Press me!")),
-            p("Source at ")(a(href := "https://github.com/AIMMOTH/iso-scala")("GitHub")),
-            div(cls := "callout alert")(h5("This is a callout"),p("It has an easy to override visual style, and is appropriately subdued."))
-          )
-        ),
-        
-        
-        // Footer
-        script(src := "/js/vendor/jquery.js"),
-        script(src := "/js/vendor/what-input.js"),
-        script(src := s"/js/vendor/foundation$min.js")
-      )
+          p("Source at ")(a(href := "https://github.com/AIMMOTH/iso-scala")("GitHub")),
+          div(cls := "callout alert")(h5("This is a callout"), p("It has an easy to override visual style, and is appropriately subdued.")))),
+
+      div(attr("ng-controller") := "SimpleController as simple")(
+        div(cls := "row")(
+          div(cls := "large-12 columns")(
+            label("Simple update"),
+            p(
+              input(`type` := "button", attr("ng-click") := "simple.inc()", value := "+"),
+              input(`type` := "button", attr("ng-click") := "simple.dec()", value := "-")),
+            p("{{ simple.simpleInt }}")))),
+
+      // Footer
+      script(src := "/js/vendor/jquery.js"),
+      script(src := "/js/vendor/what-input.js"),
+      script(src := s"/js/vendor/foundation$min.js"),
+      script(src := "https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.js"),
+      script(src := "javascript.js"),
+      script()(
+        """
+          $(document).foundation();
+          example.AngularModule().start();
+        """))
   }
 }
 
@@ -52,8 +62,8 @@ object Stylisch extends StyleSheet {
 class Builder extends HttpServlet {
 
   private val style = Stylisch
-  
-  override def doGet(request: HttpServletRequest, response: HttpServletResponse) = 
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse) =
     response.getWriter().print(Html(style.styleSheetText).render)
-    
+
 }
--- src/main/scala/example/ScalaCompiler.scala
+++ src/main/scala/example/ScalaCompiler.scala
@@ -7,17 +7,28 @@ import fiddle.ScalaJsCompiler
 import javax.servlet.http.HttpServlet
 import fiddle.Optimizer
 import scala.io.Source
+import java.util.logging.Logger
+import org.slf4j.LoggerFactory
 
 @WebServlet(name = "javascriptCompiler", urlPatterns = Array("/javascript.js"))
 class ScalaCompiler extends HttpServlet {
+
+  val log = LoggerFactory.getLogger(getClass)
   
-  override def doGet(request : HttpServletRequest, response : HttpServletResponse) = {
-    
-    val is = request.getServletContext.getResourceAsStream("/scalajs/example/Main.scala")
-    
-    val source = Source.fromInputStream(is).mkString
-    
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse) = {
+
+    import scala.collection.JavaConversions._
+
+    val sources = request.getServletContext.getResourcePaths("/scalajs/example/").filter(_.endsWith(".scala")).map {
+      file =>
+
+        log.debug(s"Adding $file to Scala JS compilation.")
+        
+        val is = request.getServletContext.getResourceAsStream(file)
+        Source.fromInputStream(is).mkString
+    }
+
     val compiler = new ScalaJsCompiler
-    response.getWriter.println(compiler.compileScalaJsString(request.getServletContext, source, Optimizer.Fast,  "/WEB-INF/lib/"))
+    response.getWriter.println(compiler.compileScalaJsStrings(request.getServletContext, sources.toList, Optimizer.Fast, "/WEB-INF/lib/", List("scalajs-angulate_sjs0.6_2.11-0.2.4.jar", "scalajs-jquery_sjs0.6_2.11-0.9.0.jar")))
   }
 }
