8107ce4b8e627ea2e0f9bdf1c95fe7dabb3e746f
Carl <carl@localhost>
2016-08-06 13:37:01 +0200

    Make resource serializable

--- src/main/webapp/scalajs/shared/Resource.scala
+++ src/main/webapp/scalajs/shared/Resource.scala
@@ -4,4 +4,6 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportAll
 
 @JSExportAll
-sealed case class Resource(val x : Integer = 0, val y : Integer = 1)
+sealed case class Resource(var x : Integer = 0, var y : Integer = 1) {
+  def this() = this(x = 0, y = 0)
+}
